007f3982ab2cfd666f86ae2b25d692947b42059a
thufir <hawat.thufir@gmail.com>
2012-04-07 22:13:45 -0700

    increment

--- src/java/net/bounceme/dur/nntp/Detail.java
+++ src/java/net/bounceme/dur/nntp/Detail.java
@@ -2,39 +2,42 @@ package net.bounceme.dur.nntp;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ManagedProperty;
-import javax.inject.Inject;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.mail.Message;
 
 @Named
-@RequestScoped
 public class Detail {
 
     private static final Logger logger = Logger.getLogger(Detail.class.getName());
     private static final Level level = Level.INFO;
-    @Inject
-    @HttpParam
-    private String id;
+    private int id = 0;
     private Message message = null;
     private SingletonNNTP nntp = SingletonNNTP.INSTANCE;
 
     public Detail() {
-        message = nntp.getMessage(Integer.parseInt(id));
+        MessageConverter mc = new MessageConverter();
+        FacesContext fc = FacesContext.getCurrentInstance();
+        String value = String.valueOf(id);
+        UIComponent ui = null;
+        Object obj = mc.getAsObject(fc, ui, value);
+        message = (Message) obj;
     }
 
-    public String forward() {
-        logger.log(level, "Detail.forward.." + id);
-        id = String.valueOf(Integer.parseInt(id) + 1);
-        logger.log(level, "..Detail.forward " + id);
+    public int forward() {
+        logger.log(level, "Detail.forward..{0}", id);
+        //id = String.valueOf(Integer.parseInt(id) + 1);
+        id = id + 1;
+        logger.log(level, "..Detail.forward {0}", id);
         return id;
     }
 
-    public String back() {
-        logger.log(level, "Detail.back.." + id);
-        id = String.valueOf(Integer.parseInt(id) - 1);
-        logger.log(level, "..Detail.back " + id);
+    public int back() {
+        logger.log(level, "Detail.back..{0}", id);
+        //id = String.valueOf(Integer.parseInt(id) - 1);
+        id = id - 1;
+        logger.log(level, "..Detail.back {0}", id);
         return id;
     }
 
