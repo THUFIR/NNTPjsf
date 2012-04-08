817e71c60e1fdae6cc86e6373f1e9073d2f35936
thufir <hawat.thufir@gmail.com>
2012-04-07 19:04:06 -0700

    SEVERE: Exception while loading the app : WELD-001408 Unsatisfied dependencies for type [FacesContext] with qualifiers [@Default] at injection point [[field] @Inject net.bounceme.dur.nntp.HttpParamProducer.facesContext] org.jboss.weld.exceptions.DeploymentException: WELD-001408 Unsatisfied dependencies for type [FacesContext] with qualifiers [@Default] at injection point [[field] @Inject net.bounceme.dur.nntp.HttpParamProducer.facesContext]

--- src/java/net/bounceme/dur/nntp/HttpParam.java
+++ src/java/net/bounceme/dur/nntp/HttpParam.java
@@ -1,12 +1,9 @@
 
 package net.bounceme.dur.nntp;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
--- src/java/net/bounceme/dur/nntp/HttpParamProducer.java
+++ src/java/net/bounceme/dur/nntp/HttpParamProducer.java
@@ -2,15 +2,13 @@ package net.bounceme.dur.nntp;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.faces.FactoryFinder;
 import javax.faces.context.FacesContext;
-import javax.faces.context.FacesContextFactory;
 import javax.inject.Inject;
 
 public class HttpParamProducer {
 
     @Inject
-    private FacesContext facesContext;
+    FacesContext facesContext;
 
     @Produces
     @HttpParam
