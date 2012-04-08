4b1d2a84e220c55a34a6e8c822bd8c1341440c40
thufir <hawat.thufir@gmail.com>
2012-04-07 17:50:54 -0700

    increment

--- src/java/net/bounceme/dur/nntp/HttpParamProducer.java
+++ src/java/net/bounceme/dur/nntp/HttpParamProducer.java
@@ -2,19 +2,28 @@ package net.bounceme.dur.nntp;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.FactoryFinder;
 import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
 import javax.inject.Inject;
 
 public class HttpParamProducer {
 
     @Inject
-    private FacesContext facesContext;
+    private FacesContext facesContext = null;
 
     @Produces
     @HttpParam
     String getHttpParameter(InjectionPoint ip) {
         String name = ip.getAnnotated().getAnnotation(HttpParam.class).value();
-        if ("".equals(name)) name = ip.getMember().getName();
-        return getFacesContext().getExternalContext().getRequestParameterMap().get(name);
+        if ("".equals(name)) {
+            name = ip.getMember().getName();
+        }
+        FacesContextFactory factory = (FacesContextFactory) FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+        String context = "context";
+        String request = "request";
+        String response = "response";
+        facesContext = factory.getFacesContext(context, request, response, null);
+        return facesContext.getExternalContext().getRequestParameterMap().get(name);
     }
 }
