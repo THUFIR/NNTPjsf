53cd40e76ade56946457b807eea0b8ecd33c8467
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-11 18:00:06 -0700

    increment

---
+++ src/java/net/bounceme/dur/nntp/Notes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.dur.nntp;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author thufir
+ */
+@Named(value = "notes")
+@Dependent
+public class Notes {
+
+    /**
+     * Creates a new instance of Notes
+     */
+    public Notes() {
+    }
+}
