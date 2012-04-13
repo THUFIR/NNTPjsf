f6f68302b57f615ff71a3f4cb008144f345e07bf
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-12 21:47:17 -0700

    increment

--- src/java/net/bounceme/dur/nntp/Detail.java
+++ src/java/net/bounceme/dur/nntp/Detail.java
@@ -5,16 +5,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ConversationScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.mail.Message;
 
 @Named
 @ConversationScoped
-public class Detail  implements Serializable {
+public class Detail implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private static final Logger logger = Logger.getLogger(Detail.class.getName());
+    private static final Logger LOG = Logger.getLogger(Detail.class.getName());
     private static final Level level = Level.INFO;
     private String id;       //should never get default value in getter
     private Message message = null;
@@ -24,44 +23,45 @@ public class Detail  implements Serializable {
     private String content = null;  //message.content
 
     public Detail() {
-        logger.log(level, "Detail..");
+        LOG.log(level, "Detail..");
     }
 
     @PostConstruct
     private void onLoad() {
-        logger.log(level, "Detail.onLoad..{0}", getId());
+        LOG.log(level, "Detail.onLoad..{0}", getId());
     }
 
-    public Message getMessage() {
-        logger.log(level, "Detail.getMessage..");
+    public Message getMessage() throws Exception {
+        LOG.log(level, "Detail.getMessage..{0}", getId());
+        message = nntp.getMessage(Integer.parseInt(getId()));
         return message;
     }
 
     public void setMessage(Message message) {
-        logger.log(level, "Detail.setMessage..");
+        LOG.log(level, "Detail.setMessage..");
         this.message = message;
     }
 
     public String getId() {
-        logger.log(level, "Detail.getId..{0}", id);
+        LOG.log(level, "Detail.getId..{0}", id);
         if (id == null) {
-            logger.log(level, "Detail.getId..SETTING DEFAULT ID");
+            LOG.log(level, "Detail.getId..SETTING DEFAULT ID"); //should never be null
             id = String.valueOf(2000);
         }
         return id;
     }
 
     public void setId(String id) throws Exception {
-        logger.log(level, "Detail.setId..{0}", getId());
+        LOG.log(level, "Detail.setId..{0}", getId());
         this.id = id;
-        logger.log(level, "..Detail.setId {0}", getId());
+        LOG.log(level, "..Detail.setId {0}", getId());
     }
 
     public String getForward() {
-        logger.log(level, "Detail.forward..");
+        LOG.log(level, "Detail.forward..");
         int f = Integer.parseInt(getId());
         f = f + 1;
-        logger.log(level, "..Detail.forward {0}", f);
+        LOG.log(level, "..Detail.forward {0}", f);
         forward = String.valueOf(f);
         return forward;
     }
@@ -71,10 +71,10 @@ public class Detail  implements Serializable {
     }
 
     public String getBack() {
-        logger.log(level, "Detail.back..");
+        LOG.log(level, "Detail.back..");
         int b = Integer.parseInt(getId());
         b = b - 1;
-        logger.log(level, "..Detail.back {0}", b);
+        LOG.log(level, "..Detail.back {0}", b);
         back = String.valueOf(b);
         return back;
     }
@@ -84,8 +84,7 @@ public class Detail  implements Serializable {
     }
 
     public String getContent() throws Exception {
-        logger.log(level, "Detail.getContent..{0}", getId());
-        message = nntp.getMessage(Integer.parseInt(getId()));
+        LOG.log(level, "Detail.getContent..{0}", getId());
         content = message.getContent().toString();
         return content;
     }
