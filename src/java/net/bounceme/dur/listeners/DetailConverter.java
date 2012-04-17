c235bd86d54d322fc576fe9bd2480385a50ef709
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-16 19:52:51 -0700

    increment

--- src/java/net/bounceme/dur/listeners/DetailConverter.java
+++ src/java/net/bounceme/dur/listeners/DetailConverter.java
@@ -1,18 +1,30 @@
 package net.bounceme.dur.listeners;
 
 import java.util.logging.Logger;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.ActionEvent;
-import javax.faces.event.ActionListener;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import net.bounceme.dur.nntp.SingletonNNTP;
 
-public class DetailConverter implements ActionListener {
+@FacesConverter("detailConverter")
+public class DetailConverter implements Converter {
 
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = Logger.getLogger(DetailConverter.class.getName());
+    private SingletonNNTP nntp = SingletonNNTP.INSTANCE;
 
     @Override
-    public void processAction(ActionEvent event) throws AbortProcessingException {
-        LOG.info("DetailListener.processAction..");
-        LOG.info("..DetailListener.processAction");
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+       throw new UnsupportedOperationException("get as object Not supported yet.");
+
     }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        throw new UnsupportedOperationException("get as string Not supported yet.");
+    }
+
+
+
 }
