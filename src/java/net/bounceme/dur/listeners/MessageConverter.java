e9593bdd3bd3a5f1e06654dbefa66e5382d866e6
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-16 21:21:01 -0700

    increment

--- src/java/net/bounceme/dur/listeners/MessageConverter.java
+++ src/java/net/bounceme/dur/listeners/MessageConverter.java
@@ -9,32 +9,35 @@ import javax.faces.convert.FacesConverter;
 import javax.mail.Message;
 import net.bounceme.dur.nntp.SingletonNNTP;
 
-@FacesConverter("detailConverter")
-public class DetailConverter implements Converter {
+@FacesConverter("messageConverter")
+public class MessageConverter implements Converter {
 
     private static final long serialVersionUID = 1L;
-    private static final Logger LOG = Logger.getLogger(DetailConverter.class.getName());
+    private static final Logger LOG = Logger.getLogger(MessageConverter.class.getName());
     private SingletonNNTP nntp = SingletonNNTP.INSTANCE;
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        LOG.info("MessageConverter.getAsObject..");
         Message message = null;
         try {
             message = nntp.getMessage();
         } catch (Exception ex) {
-            Logger.getLogger(DetailConverter.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MessageConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
         return message;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
+        LOG.info("MessageConverter.getAsString..");
         Message message = null;
         try {
             message = nntp.getMessage();
         } catch (Exception ex) {
-            Logger.getLogger(DetailConverter.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MessageConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
+        LOG.info("MessageConverter.getAsString.." + message.getMessageNumber());
         return String.valueOf(message.getMessageNumber());
     }
 }
