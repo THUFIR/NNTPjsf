0df76360ee0c21d69aabf4fa53ab42548f9c8002
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-17 03:18:45 -0700

    increment

--- src/java/net/bounceme/dur/beans/MessageBean.java
+++ src/java/net/bounceme/dur/beans/MessageBean.java
@@ -8,6 +8,7 @@ import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.mail.Message;
+import net.bounceme.dur.nntp.SingletonNNTP;
 
 @Named
 @ConversationScoped
@@ -15,36 +16,68 @@ public class MessageBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = Logger.getLogger(MessageBean.class.getName());
-    @Named
-    @Produces
     private Message message;
     private URL url = null;
+    private int id = 0;
+    private int previous = 0;
+    private int next = 0;
 
     public MessageBean() {
-        LOG.info("Message..");
+        LOG.info("MessageBean..");
     }
+
     @PostConstruct
     public void postConstruct() throws Exception {
-        LOG.info("Message.postConstruct..");
-        MessageUtils.getUrl(message);
+        LOG.info("MessageBean.postConstruct..");
+        SingletonNNTP nntp = SingletonNNTP.INSTANCE;
+        nntp.setIndex(id);
+        setMessage(nntp.getMessage());
+        setId(message.getMessageNumber());
+        setPrevious(getId() - 1);
+        setNext(getId() + 1);
+        setUrl(MessageUtils.getUrl(message));
     }
 
     public Message getMessage() {
-        LOG.info("Message.getMessage..");
+        LOG.info("MessageBean.getMessage..");
         return message;
     }
 
     public void setMessage(Message message) {
-        LOG.info("Message.setMessage..");
+        LOG.info("MessageBean.setMessage..");
         this.message = message;
     }
 
     public URL getUrl() throws Exception {
-        LOG.info("Messages.getUrl..");
+        LOG.info("MessageBean.getUrl..");
         return url;
     }
 
     public void setUrl(URL url) {
         this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(int previous) {
+        this.previous = previous;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
     }
 }
