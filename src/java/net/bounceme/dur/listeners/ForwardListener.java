8637b8c2e7265f8cb5fd478bfb8b7257bba3f655
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-16 23:04:10 -0700

    increment

--- src/java/net/bounceme/dur/listeners/ForwardListener.java
+++ src/java/net/bounceme/dur/listeners/ForwardListener.java
@@ -6,20 +6,20 @@ import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 import net.bounceme.dur.nntp.SingletonNNTP;
 
-public class BackListener implements ActionListener {
+public class ForwardListener implements ActionListener {
 
     private static final long serialVersionUID = 1L;
-    private static final Logger LOG = Logger.getLogger(BackListener.class.getName());
+    private static final Logger LOG = Logger.getLogger(ForwardListener.class.getName());
 
     @Override
     public void processAction(ActionEvent event) throws AbortProcessingException {
         try {
-            LOG.info("BackListener.processAction..." + event.toString());
+            LOG.info("ForwardListener.processAction..." + event.toString());
             SingletonNNTP nntp = SingletonNNTP.INSTANCE;
-            nntp.page(true);
+            nntp.page(false);
         } catch (Exception ex) {
-            LOG.severe("..BackListener.processAction would not execute " + ex.getMessage());
+            LOG.severe("..ForwardListener.processAction would not execute " + ex.getMessage());
         }
-        LOG.info("..BackListener.processAction");
+        LOG.info("..ForwardListener.processAction");
     }
 }
